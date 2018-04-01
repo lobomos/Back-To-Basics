c1c4903eb83b304ea9e9bda809d2cfcb00a535ef
deca <1048804+lobomos@users.noreply.github.com>
2018-03-31 20:12:28 -0500

    Added a bit more to double linked list

--- Java/Lists/MyDoubleLinkedList.java
+++ Java/Lists/MyDoubleLinkedList.java
@@ -35,6 +35,37 @@ public class MyDoubleLinkedList {
     public boolean isEmpty() {
         return this.size == 0;
     }
+
+    public int[] toArray() {
+        int[] output = new int[this.size];
+        Node temp = this.head;
+        if(temp == null) {
+            return null;
+        }
+        int counter = 0;
+        while(temp.next != null) {
+            output[counter] = temp.data;
+            counter++;
+            temp = temp.next;
+        }
+        output[this.size-1] = temp.data;
+        return output;
+    }
+
+    public String toString() {
+        String output = "";
+        Node temp = this.head;
+        if(temp == null) {
+            return output;
+        }
+        while(temp.next != null) {
+            output += temp.data + " ";
+            temp = temp.next;
+        }
+        // This avoids having an additional space on the end.
+        output += temp.data;
+        return output;
+    }
     
     private class Node {
         
@@ -46,5 +77,26 @@ public class MyDoubleLinkedList {
             this.next = null;
             this.prev = null;
         }
+    }
+
+    public static void main(String[] args) {
+        MyDoubleLinkedList test = new MyDoubleLinkedList();
+        test.add(3);
+        test.add(5);
+        test.add(7);
+        System.out.println("toString Test: " + test.toString());
+        System.out.println("size Test: " + test.size());
+        int[] testArray = test.toArray();
+        System.out.println("toArray Test: ");
+        for(int i = 0; i < testArray.length; i++) {
+            System.out.print(testArray[i] + " ");
+        }
+        System.out.println();
+        System.out.println("isEmpty Test: " + test.isEmpty());
+        MyDoubleLinkedList empty = new MyDoubleLinkedList();
+        System.out.println(empty.toString());
+        System.out.println(empty.size());
+        System.out.println(empty.toArray());
+        System.out.println(empty.isEmpty());
     }
 }
